4bf612cdb1116f8e20f28990433ca4d68103d3f5
arcady <arcad.khrulev@gmail.com>
2021-01-24 14:08:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+/*var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(person.name);
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
+console.log(arr[0]); */
+
+//alert("Hello world");
+
+//confirm("Are you here?");
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18", "Да");
+//console.log(typeof(answer));
+//console.log(typeof(answer));
+//console.log(typeof(arr));
+//console.log(4 + +"- object");
+
+//let incr = 10,
+  //  decr = 10;
+
+//console.log(incr++);
+//console.log(++incr);
+//console.log(decr--);
+//console.log(--decr);
+
+//console.log(5%2);
+//console.log("2" === 2);
+
+
+
+
+
