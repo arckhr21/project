7136574137dcdea356699df0c5b0684cebba0d70
arcady <arcad.khrulev@gmail.com>
2021-01-24 20:59:52 +0300

    first learn

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
 'use strict';
 
-let a = 5;
-console.log(a);
 
-/*var LeftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-console.log(second);
-
-let person = {
-    name: "John",
-    age: 25,
-    isMarried: false
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
-console.log(person.name);
-console.log(person["name"]);
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[2]);
-console.log(arr[0]); */
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-//alert("Hello world");
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-//confirm("Are you here?");
-
-//let answer = confirm("Are you here?");
-//console.log(answer);
-
-//let answer = +prompt("Есть ли вам 18", "Да");
-//console.log(typeof(answer));
-//console.log(typeof(answer));
-//console.log(typeof(arr));
-//console.log(4 + +"- object");
-
-//let incr = 10,
-  //  decr = 10;
-
-//console.log(incr++);
-//console.log(++incr);
-//console.log(decr--);
-//console.log(--decr);
-
-//console.log(5%2);
-//console.log("2" === 2);
-
-
-
-
+alert(appData.budget / 30);
 
