6cc94fefdb413f316638b331441b4939ccd8753b
arcady <arcad.khrulev@gmail.com>
2021-03-17 16:01:30 +0300

    Урок 4-7. Калькулятор

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -259,6 +259,51 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+    //Calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personSum = 0,
+        daysSum = 0,
+        total = 0;
+    
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function() {
+        personSum = +this.value;                        //"+" преобразует сроку в число
+        total = (daysSum + personSum)*4000;
+
+        if(restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (daysSum + personSum)*4000;
+
+        if(restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a*this.options[this.selectedIndex].value;
+        }
+    });
+
+
+
     
  
 });  //End Window
